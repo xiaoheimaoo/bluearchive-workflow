96e102f9df94f060d4d4d653779c4691010ee892
小黑猫 <737476225@qq.com>
2026-02-23 10:11:11 +0800

    Update decompile.sh

--- scripts/decompile.sh
+++ scripts/decompile.sh
@@ -2,9 +2,11 @@
 set -euo pipefail
 
 ./.tools/Il2CppInspector.Redux.CLI -i ./libil2cpp.so -m global-metadata.dat \
-    -l tree -c DiffableCs \
-    -o script.json -d DummyDll \
-    --select-outputs cs json dll
+  -l tree \
+  -c DiffableCs \
+  -o script.json \
+  -d DummyDll \
+  --select-outputs
 
 echo "Extracting protocolConverter VA offset value..."
 offset=$(rg "^\s+public int TypeConversion\(uint crc, Protocol protocol\);.*// (0x[0-9A-F]+)-" -o --no-filename -r '$1' ./DiffableCs/BlueArchive/MX/NetworkProtocol/ProtocolConverter.cs)
